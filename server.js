f752e1b2e460439f1a50b2c0a20d85a34f4d7b98
Yuriy Pevzner <yuriy.pevzner@todor3d.com>
2023-04-03 00:53:39 +0200

    test redirection

--- server.js
+++ server.js
@@ -17,14 +17,22 @@ const app = express();
 const PORT = 3001;
 
 let count = 0
+// console.log(path.join(__dirname, 'frontend', 'ASH', 'dist'))
 
 app.listen(PORT, (error)=>{
+    // console.log(path.join(__dirname, 'frontend', 'ASH', 'dist'))
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
 
 app.get('/order', (req, res)=>{
     count = count + 1
     bot.sendMessage(YURA, `NEW ЗАМОВЛЕННЯ ${count}`)
+})
+app.get('/', (req, res)=>{
+    res.sendFile('./build/index.html', {root: __dirname })
+})
+app.get('/ash', (req, res)=>{
+    res.sendFile(path.join (__dirname, '..', '..', 'frontend', 'ASH', 'dist'))
 })
 
 
