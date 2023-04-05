25385aebe3730bdaa74f189dbabe974c07b7b04b
Yuriy Pevzner <yuriy.pevzner@todor3d.com>
2023-04-05 11:59:13 +0200

    back + front

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const TelegramApi = require('node-telegram-bot-api');
 const token ='6077774598:AAEJJaRdxoFdvp_A_RIn7CrOeS9nIqj1Zmw'
 
-const bot = new TelegramApi(token, {polling: true})
+// const bot = new TelegramApi(token, {polling: true})
 
 
 
@@ -17,31 +17,47 @@ const app = express();
 const PORT = 3001;
 
 let count = 0
-// console.log(path.join(__dirname, 'frontend', 'ASH', 'dist'))
 
 app.listen(PORT, (error)=>{
-    // console.log(path.join(__dirname, 'frontend', 'ASH', 'dist'))
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
 
-app.get('/order', (req, res)=>{
+app.get('/api/send-order', (req, res)=>{
     count = count + 1
-    bot.sendMessage(YURA, `NEW ЗАМОВЛЕННЯ ${count}`)
-})
-app.get('/', (req, res)=>{
-    res.sendFile('./build/index.html', {root: __dirname })
-})
-app.get('/ash', (req, res)=>{
-    res.sendFile(path.join (__dirname, 'build', 'index.html'))
+    console.log('send message')
+    // bot.sendMessage(YURA, `NEW ЗАМОВЛЕННЯ ${count}`)
 })
 
 
 
-bot.on('message', msg=>{
-    const text = msg.text;
-    const id = msg.chat.id;
 
-    if(text === '/start'){
-        bot.sendMessage(id, `Добро пожаловать в ASH`)
-    }
-})
+// app.use('/api', router);
+
+app.use(express.static(`${__dirname}/../../client/ASH/build`));
+
+app.get('*', (_, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type, Authorization',
+  );
+
+  res.contentType('text/html; charset=utf-8');
+
+  res.sendFile(path.join(`${__dirname}/../../client/ASH/build/index.html`));
+});
+
+
+
+
+
+
+// bot.on('message', msg=>{
+//     const text = msg.text;
+//     const id = msg.chat.id;
+
+//     if(text === '/start'){
+//         bot.sendMessage(id, `Добро пожаловать в ASH`)
+//     }
+// })
